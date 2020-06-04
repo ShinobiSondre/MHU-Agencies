5d60ff280ca80a73797fb5d1fc030d03c76de2b8
ShinobiSondre <48123393+ShinobiSondre@users.noreply.github.com>
2020-06-04 19:48:11 +0200

    Working Intelliji Project

--- src/main/java/mhuagencies/mhuagencies/MHUAgencies.java
+++ src/main/java/mhuagencies/mhuagencies/MHUAgencies.java
@@ -17,7 +17,7 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MHUAgencies extends JavaPlugin implements Listener {
+public final class MHUAgencies extends JavaPlugin implements Listener,CommandExecutor {
 
     double expsharescore;
     double previousscore;
@@ -30,12 +30,45 @@ public final class MHUAgencies extends JavaPlugin implements Listener {
 
         System.out.print("--------------------------------------" + "\n" + "MHU-Agencies" + "\n" + "--------------------------------------");
 
-        this.getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
+        this.getCommand("agencydefenders").setExecutor(new MHUAgencies());
     }
 
 
 
-    @EventHandler
+    public ActiveMob spawnDefenders(String MobName, String killer) {
+
+        MobManager mm = MythicMobs.inst().getMobManager();
+
+        Location loc1 = new Location(Bukkit.getPlayer(killer).getWorld(), Bukkit.getPlayer(killer).getLocation().getX() + 4, Bukkit.getPlayer(killer).getLocation().getY(), Bukkit.getPlayer(killer).getLocation().getZ() + 4);
+
+        return mm.spawnMob(MobName, loc1);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player player = (Player) sender;
+
+        if (command.getName().equalsignorecase("agencydefenders")) {
+
+            String[] mobs = {"D1,D2,D3,D4"};
+
+            if(mobs.equals("")){
+
+            }
+
+            for (int i = 0; i<5; i++) {
+                spawnDefenders(mobs[i],player.getName());
+            }
+            player.sendMessage("The Defenders have been placed");
+
+        }
+
+        return false;}
+
+
+        @EventHandler
     public void AddExp(MythicMobDeathEvent d) throws InterruptedException {
 
 
